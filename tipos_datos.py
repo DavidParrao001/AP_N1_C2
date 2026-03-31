f047b9a71a56e449858dd693a9566bf0c38c6793
DavidParrao001 <david.parra@lasamericastemuco.cl>
2026-03-31 10:53:58 -0300

    aqui va la primera

---
+++ tipos_datos.py
@@ -0,0 +1,21 @@
+#tipos de datos en Python
+
+numero_real = 0
+
+numero_decimal = 0.0
+
+cadena_de_texto = "hola querido hijo de su madre"
+
+print ("mi primer mensaje de python")
+
+print (numero_real)
+
+print (type(numero_real))
+
+print (numero_decimal)
+
+print (type(numero_decimal))
+
+print (cadena_de_texto)
+
+print (type(cadena_de_texto))
