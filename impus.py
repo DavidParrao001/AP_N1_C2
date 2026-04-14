e7583fe7094366adcff423fb3563106a9f727bef
DavidParrao001 <david.parra@lasamericastemuco.cl>
2026-04-14 10:07:41 -0400

    aqui va

--- impus.py
+++ impus.py
@@ -1,8 +1,9 @@
-#Cada rato ingresado mediante input ES un TEXTO
+# Cada dato ingresado medainte input ES un TEXTO
 
-numero = input('Por favor, ingrese un numero entero')
+numero = input('Por favor, ingrese un númereo entero...')
 print(type(numero))
 print(numero)
 
-print(f'Su NÚMERO multiplicado por 4 es :  {numero * 4}')
-print(f'Su NÚMERO
+print(f'Su NÚMERO multiplicado por 4 es : {numero * 4}')
+print(f'Su NÚMERO (convertido a decimal) multiplicado por 2 es : {float(numero) * 2}')
+print(f'Su NÚMERO (convertido a entero) multiplicado por 2 es : {int(numero) * 2}')
