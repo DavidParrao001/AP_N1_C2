077bf7b3a988ca6d71e3f427c35b9db8d4167864
DavidParrao001 <david.parra@lasamericastemuco.cl>
2026-04-22 10:21:12 -0400

    y que te digo

---
+++ ciclos/ciclo_por_conjuntos.py
@@ -0,0 +1,13 @@
+
+
+conjunto_numero = {10,20,30,40,50}
+
+for numero in conjunto_numero:
+    print(numero)
+
+for elemento in conjunto_numero:
+    resultado = elemento * 100
+    print(resultado)
+
+    print()
+    print(lista_numeros)
