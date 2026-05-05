1fa6fdf6e76eb415366633a9e6d5e66351ade30e
DavidParrao001 <david.parra@lasamericastemuco.cl>
2026-05-05 10:56:11 -0400

    operadores

---
+++ operadores/operadores_comparacion.py
@@ -0,0 +1,24 @@
+print ('Operador EQUIVALENTE')
+print(f'2 == 3 ? {2 == 3}')
+print (F'6 == 12/2 ? {6 == 12/2}')
+
+print('\n Operador MAYOR QUE')
+
+print(f'2 > 3? {2 > 3}')
+print (F'6 > 12/2 ? {6 > 12/2}')
+
+print('\n Operador MAYOR O IGUAL QUE')
+
+print(f'2 >= 3? {2 >= 3}')
+print (F'6 >= 12/2? {6 >= 12/2}')
+
+print('\n Operador MENOS QUE')
+
+print(f'2 < 3? {2 < 3}')
+print (F'6 < 12/2? {6 < 12/2}')
+
+print('\n Operador MENOS O IGUAL QUE')
+
+print(f'2 <= 3? {2 <= 3}')
+print (F'6 <= 12/2? {6 <= 12/2}')
+
---
+++ operadores/operadores_logicos.py
@@ -0,0 +1,12 @@
+print('/n Operador logico O (or)')
+print(f'V o V = {True or True}')
+print(f'V o F = {True or False}')
+print(f'F o V = {False or True}')
+print(f'F o F = {False or False}')
+
+
+print('/n Operador logico Y (and)')
+print(f'V and V = {True and True}')
+print(f'V and F = {True and False}')
+print(f'F and V = {False and True}')
+print(f'F and F = {False and False}')
