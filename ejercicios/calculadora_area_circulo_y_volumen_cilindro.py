dc09cd3de5530fa8bc39244d1a03618af8c65d24
DavidParrao001 <david.parra@lasamericastemuco.cl>
2026-05-06 10:50:15 -0400

    ejercicios de eva

--- ejercicios/calculadora_area_circulo_y_volumen_cilindro.py
+++ ejercicios/calculadora_area_circulo_y_volumen_cilindro.py
@@ -1,43 +1,96 @@
+from math import pi as pi
 
-import math
-from calculadora import convertir_float
+# 1.- Escriba una función que calcule el total de una factura tras aplicarle el IVA.
+#       La función debe recibir la cantidad sin IVA y el porcentaje de IVA a aplicar,
+#       si la función no recibe el porcentaje de IVA, deberá aplicar por defecto un 19%
 
-def area_circulo(Radio):
-   resultado = PI * Radio**2
-   print(f'el area del circulo es = {resultado}')
-   return
-        
-def volumen_cilindro(altura,Radio):
-    area = area_circulo(Radio)
-    resultado = Radio * altura
-    return
+def pedir_datos_factura():
+    str_monto = input('Ingrese el monto de la factura: ')
+    str_procentaje = input('Ingrese el pordentaje de IVA\n(o deje en blanco para aplicar 19%): ')
+    if str_procentaje == '':
+        str_procentaje = '19'
+    monto = convertir_float(str_monto)
+    porcentaje = convertir_float(str_procentaje)
+    factura(monto,porcentaje)
 
-def calculo_cilindro():
-   print('Ingrese los datos solicitados')
-   str_radio = input("radio de cilindro: ")
-   str_radio = input("volumen de cilindro: ")
-   radio = calculadora.convertir_float(str_radio)
-   altura = calculadora.convertir_float(str_radio)
-   volumen = volumen_cilindro(radio,altura)
-   print(volumen)
+def factura(monto,porcentaje):
+    if monto and porcentaje != None:
+        monto_iva = monto * (porcentaje/100)
+        total = monto + monto_iva
+        print()
+        print(f'Monto Factura: ${round(monto)}')
+        print(f'Monto IVA: ${round(monto_iva)}')
+        print(f'% IVA aplicado: {round(porcentaje)}')
+        print(f'Total Factura: ${round(total)}')
+    else:
+        print('valores ingresados NO corresponden.')
 
+# 2.- Escriba una función que calcule el área de un círcul
+def area_circunferencia(radio):
+    resultado = pi * radio * radio
+    return resultado
 
+def volumen_cilindro(radio,altura):
+    area = area_circunferencia(radio)
+    resultado = area * altura
+    return resultado
 
+def calculo_volumen_cilindro():
+    print('Ingrese los datos solicitados')
+    str_radio = input('Radio Cilindro: ')
+    str_altura = input('Altura Cilindro: ')
+    radio = convertir_float(str_radio)
+    altura = convertir_float(str_altura)
+    if radio and altura != None:
+        volumen = volumen_cilindro(radio,altura)
+        print()
+        print(f'El VOLUMEN de un cilindro de radio {radio} y altura {altura} = {volumen}')
 
-print("-----------------------------------------------------------")
-print("calculadora de area de circulo y volumen de cilindro")
-print("===================================================")
+# 3.- Escriba una función que permita escribir la tabla de multiplicar de un número 
+# ingresado por el usuario.
 
-print ("holaa, que vas a querer hacer el dia de hoy?")
+def tabla():
+    str_numero = input('Ingrese un número: ')
+    numero = convertir_float(str_numero)
+    factor = 1
+    print()
+    if numero != None:
+        while factor <= 12:
+            total = numero * factor
+            print(f'{numero} x {factor} = {total}')
+            factor += 1
+
+# Funciones auxiliares
+def convertir_float(valor):
+    try:
+        return float(valor)
+    except ValueError:
+        print('El valor ingresado NO se puede convertir a número.')
+    except Exception as error:
+        print(f'Se ha producido el siguiente error: {error}')
+        print(f'Tipo de error: {type(error)}')
+        print(f'Argumentos del error: {error.args}')
+        return None
+
 while True:
- print('[1] calcular area de un circulo')
- print('[2] calcular el volumen')
- opcion = input('\nSeleccione su operación [0-2]: ')
+    print()
+    print('[1] Cálculo IVA')
+    print('[2] Cálculo Volumen Cilindro')
+    print('[3] Tabla de Multiplicar')
+    print('[0] Salir')
 
- if opcion == '1':
-    calculo_cilindro
- elif opcion == '2':
-    area_circulo(Radio)
+    opcion = input('Ingrese su Opción [0-3]')
+    valores_opcion = ['0','1','2','3']
 
- else:
-    print('Opción NO válida.')
+    if opcion in valores_opcion:
+        if opcion == '1':
+            pedir_datos_factura()
+        elif opcion == '2':
+            calculo_volumen_cilindro()
+        elif opcion == '3':
+            tabla()
+        elif opcion == '0':
+            print('Saliendo del sistema...')
+            break
+    else:
+        print('Opción ingresada NO corresponde...')
